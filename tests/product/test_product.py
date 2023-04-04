7f1ea4777fbc475f99b5ff0cfb783199c07f1432
reinaldoper <reinaldoper83@gmail.com>
2023-04-04 16:43:30 -0400

    feat: requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,33 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from datetime import date
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    result = Product(
+        id=1,
+        nome_do_produto="Milk Shake",
+        nome_da_empresa="SoveteCia",
+        data_de_fabricacao=date(2023, 2, 5),
+        data_de_validade=date(2023, 3, 5),
+        numero_de_serie="54321",
+        instrucoes_de_armazenamento="em local com temperatura abaixo de 10°C",
+    )
+
+    assert result.id == 1
+    assert result.nome_do_produto == "Milk Shake"
+    assert result.nome_da_empresa == "SoveteCia"
+    assert result.data_de_fabricacao == "2023-02-05"
+    assert result.data_de_validade == "2023-03-05"
+    assert result.numero_de_serie == "54321"
+    assert (
+        result.instrucoes_de_armazenamento
+        == "em local com temperatura abaixo de 10°C"
+    )
+    result_function_repr = (
+        "O produto Milk Shake"
+        " fabricado em 2023-02-05"
+        " por SoveteCia com validade"
+        " até 2023-03-05"
+        " precisa ser armazenado em local com temperatura abaixo de 10°C."
+    )
+    assert repr(result) == result_function_repr
