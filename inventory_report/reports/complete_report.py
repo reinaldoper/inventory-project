af409271afff9d9d8109f5008d251aab44caa862
reinaldoper <reinaldoper83@gmail.com>
2023-04-04 21:21:56 -0400

    feat: requisito 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,20 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, data):
+        list_empresa = Counter(
+            product["nome_da_empresa"] for product in data)
+
+        list_header = "Produtos estocados por empresa:"
+        products = ""
+        for line in list_empresa:
+            products += f"- {line}: {list_empresa[line]}\n"
+
+        return (
+            f"{super().generate(data)}\n"
+            f"{list_header}\n"
+            f"{products}"
+        )
